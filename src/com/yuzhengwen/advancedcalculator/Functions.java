9bc2586b1a99f221a4a21b7ad8d1f1ab9e68d226
yuzhengwen <yuzhengwen0217@gmail.com>
2015-12-16 21:42:07 +0800

    fixed some small bugs in basic functions

--- src/com/yuzhengwen/advancedcalculator/Functions.java
+++ src/com/yuzhengwen/advancedcalculator/Functions.java
@@ -1,27 +1,36 @@
 package com.yuzhengwen.advancedcalculator;
 
-import android.util.Log;
-
 public class Functions {
 
 	public static double add(double x, double y) {
-		Log.d("TAG", "ADD");
 		return x + y;
 	}
 
 	public static double minus(double x, double y) {
-		Log.d("TAG", "MINUS");
-		return x + y;
+		return x - y;
 	}
 
 	public static double multiply(double x, double y) {
-		Log.d("TAG", "MULTIPLY");
-		return x + y;
+		return x * y;
 	}
 
 	public static double divide(double x, double y) {
-		Log.d("TAG", "DIVIDE");
-		return x + y;
+		return x / y;
+	}
+
+	//advanced functions
+	
+	public static double squareRoot(double x) {
+		return Math.sqrt(x);
+	}
+
+	public static double cubeRoot(double x) {
+		return Math.cbrt(x);
+	}
+
+	/** x to the power of y */
+	public static double powerOf(double x, double y) {
+		return Math.pow(x, y);
 	}
 
 }
